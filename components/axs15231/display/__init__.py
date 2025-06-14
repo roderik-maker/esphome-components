b2227f759ea59259915669734a5412f607e98c87
Roderik <69784744+roderik-maker@users.noreply.github.com>
2025-06-14 22:17:31 +0200

    Update __init__.py

--- components/axs15231/display/__init__.py
+++ components/axs15231/display/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components.spi import QuadSPIComponent
 
 from esphome import pins
 from esphome.components import (
